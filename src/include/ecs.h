92a2e0de83a84e635eacee9f74ffa2f155641c8b
bonbonbaron <bonbonbaron@gmail.com>
2021-09-14 17:02:24 -0400

    Update ecs.h

--- src/include/ecs.h
+++ src/include/ecs.h
@@ -1,38 +1,8 @@
 #ifndef ECS_H
 #define ECS_H
 #include "data.h"
-#include "errors.h"
 
-/* This is how every system increments the component pointer. */
-#define INCR_CMP_P(ptr, stepSz) (((U8*) ptr) += stepSz)
-#define INI_CMP_P(ptr) (((U8*) ptr) + sizeof(Entity))
-
-/* Entity */
-typedef struct {
-	Component *componentsA;
-} EntitySeed;
-
-typedef U8 Entity;  /* HAH! That's all an entity is!? A key to its components like we're a key to our atoms! */
-
-/* Component */
-typedef struct {
-	U8 sysKey;
-	U8 cmpDataSz;  /* this size does not include the Entity byte */
-	void *cmpDataP;
-	Map *cmpMapP;  /* For things like animation, this is crucial. We won't copy entire maps to a system; just the cmps' init vals. */
-} ComponentSeed;
-
-typedef struct {
-	Entity owner;
-	void *data;  /* actual data component entails, like Velocity */
-} Component;
-
-/* System */
-struct _System;
-typedef Error (*SysFP)(struct _System *sysP, void *cmpA);
-typedef Error (*SysIniFP)(struct _System *sysP, void *cmpA);
-typedef void (*SysBasicFP)(System *sP);
-
+/* Enums */
 typedef enum {
 	DIRECTOR_SYS_KEY,
 	NUM_SYS_KEYS  // only used for creating system histograms
@@ -51,15 +21,53 @@ typedef enum {
 } MsgUrgency;
 
 typedef enum {
+  TOP_PRIORITY
+} Priority;
+
+typedef enum {
 	SYSTEMWIDE_CMD,
 	REPEATING_CMP_CMD,
 	ONE_OFF_CMP_CMD
 } CmdType;
 
+/* This is how every system increments the component pointer. */
+#define INCR_CMP_P(ptr, stepSz) (((U8*) ptr) += stepSz)
+#define INI_CMP_P(ptr) (((U8*) ptr) + sizeof(Entity))
+
+struct _Component;
+
+/**********/
+/* Entity */
+/**********/
+typedef struct {
+	struct _Component *componentsA;
+} EntitySeed;
+
+typedef U8 Entity;  /* HAH! That's all an entity is!? A key to its components like we're a key to our atoms! */
+
+/*************/
+/* Component */
+/*************/
+typedef struct {
+	U8 sysKey;
+	U8 cmpSz;  /* this size does not include the Entity byte */
+	void *cmpDataP;
+	Map *cmpMapP;  /* For things like animation, this is crucial. We won't copy entire maps to a system; just the cmps' init vals. */
+} ComponentSeed;
+
+/**********/
+/* System */
+/**********/
+struct _System;
+typedef Error (*SysFP)(struct _System *sysP, void *cmpA);
+typedef Error (*SysIniFP)(struct _System *sysP, void *cmpA);
+typedef void (*SysBasicFP)(struct _System *sP);
+
 typedef struct {
 	U8 activityIdx;
-	U8 cmpIdx;
-} CmpLocation;
+	U8 ecIdx;
+  void *ecP;      /* Systems that use pointers to other systems' components may use double pointers to avoid requesting updated info. */
+} ECLocation;
 
 typedef struct {
 	U8						 cmd;
@@ -77,15 +85,17 @@ typedef struct {
 	U8 firstInactiveIdx; /* marks the first inactive element's index */
 	U8 firstEmptyIdx; /* marks the first empty element's index */
 	SysFP *sysFP; /* function that runs on these components */
-	void *cmpsA;  /* components the above function operates on */
+	void *ecA;  /* components the above function operates on */
 } Activity;
 
 /* Systems are allocated. */
 typedef struct _System {
+  U8           id;                /* this is needed to ensure messages are sent to the correct system */
 	U8           active;            /* boolean for whether this syskem should even operate */
-	U8           cmpStepSz;             /* components are the same size in all of this system's activities */
+	U8           ecSz;              /* components are the same size in all of this system's activities */
+  void        *swapPlaceholderP;  /* An array to hold the place of swaps; avoids allocating a new placeholder every time. */
 	Map         *cmpLocationMapP;   /* maps component IDs to an element in an array of CmpAddresses */
-	SysActivity *activityA;
+	Activity    *activityA;
 	Message     *inboxA;
 	Message     *outboxA;
 } System;
@@ -95,8 +105,6 @@ typedef enum {
 	_MAX_SYSTEMWIDE_CMD_ID
 } SysCmdID;
 
-SysBasicFP sysBasicFuncs[] = {
-	sysToggleActive
-};
+SysBasicFP sysBasicFuncs[];
 
 #endif
