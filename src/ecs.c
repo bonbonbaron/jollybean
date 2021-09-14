f1fc2f143988d0887ce24688a66bb4da6c860f37
bonbonbaron <bonbonbaron@gmail.com>
2021-09-14 17:00:24 -0400

    Update ecs.c

--- src/ecs.c
+++ src/ecs.c
@@ -1,59 +1,136 @@
 #include "ecs.h"
 
-
 /* Components in Arrays: Thought Process
  *     ecs.c sees entities and components as a unit. System *implementations*, however, usually see only the components. They do this by jumping over the entities as they loop through. The components are pulled out of storage and lumped together with entities as they enter systems' bowels. They swap together, move together, delete together; they always stay together. */
 
 /* Only inspect every possible messaging error at load-time. Do not inspect every single message-reading. */
-inline static U8 _validateActivityIdx(const System *sP, const U8 activityIdx) { 
+__inline static U8 _validateActivityIdx(const System *sP, const U8 activityIdx) { 
 	return (activityIdx < (U8) arrayGetNElems(sP->activityA));
 }
 
-/* I repeat; Only inspect every possible messaging error at load-time. Do not inspect every single message-reading. */
-inline static U8 _validateCmpIdx(const System *sP, const U8 activityIdx, const U8 cmpIdx) { 
-	return (cmpIdx < (U8) arrayGetNElems(sP->activityA[activityIdx].cmpsA);
+/* Only inspect every possible messaging error at load-time. Do not inspect every single message-reading. */
+__inline static U8 _validateEcIdx(const System *sP, Activity *aP, const U8 ecIdx) { 
+	return (ecIdx < (U8) arrayGetNElems(aP->ecA));
 }
 
-/* Wait... does this mean that every single component needs to have an entity member? If I store a Velocity in the database, I write a MotionSystem to work with Velocity components. It knows to jump, and ecs.c doubtless comes packaged with an inlined "getEntity()" utility. So no, it doesn't need an Entity member; they just get injected awkwardly between the gaps of the components by ecs.c lol. */
+/* EC Getters and Setters */
+/* When activating or deactivating, you have to update the system's directory to its new location. */
+__inline static Entity _getEntity(const void *cmpP) {
+	return *((Entity*) cmpP);
+}
 
-static void iniActivity(const Activity *aP, const SysFP *funcPtr) {
+/* This may be unnecessary. We'll see. */
+__inline static void* _getComponentP(const void *cmpP) {
+	return ((U8*) cmpP) + (sizeof(Entity));
+}
+
+__inline static void* _getEcPByIndex(const System *sP, Activity *aP, U8 ecIdx) {
+  return (void*) (((U8*) aP->ecA) + (sP->ecSz * ecIdx));
+}
+
+/* Returns a component's location in the jagged array of components. Each row in the J-array corresponds to a function. */
+__inline static ECLocation* _getECLocation(System *sP, Entity entity) {
+	return (ECLocation*) mapGet(sP->cmpLocationMapP, entity);
+}
+
+/* Checks if the component, wherever it is in the jagged array, is before the function's stopping point in its array. */
+__inline static U8 _cmpIsActive(const System *sP, const ECLocation *eclP) {
+	return (eclP->ecIdx < sP->activityA[eclP->activityIdx].firstInactiveIdx);
+}
+
+__inline static _updateECLocation(ECLocation *eclP, U8 dstActIdx, U8 dstEcIdx, void *dstEcP) {
+  eclP->activityIdx = dstActIdx;
+  eclP->ecIdx       = dstEcIdx;
+  eclP->ecP         = dstEcP;
+}
+
+static void _mvEC(System *sP, void *srcEcP, U8 dstActIdx, U8 dstEcIdx) {
+  /* First, move the component to its destination. */
+  ECLocation *eclP = _getECLocation(sP, _getEntity(srcEcP));
+  void *dstEcP = _getEcPByIndex(sP, &sP->activityA[dstActIdx], dstEcIdx);
+  memcpy(dstEcP, eclP, sP->ecSz);
+  /* Then update its location in the system's EC directory. */
+  _updateECLocation(eclP, dstActIdx, dstEcIdx, dstEcP);
+}
+
+static void _swapECs(System *sP, void *srcEcP, U8 dstActIdx, U8 dstEcIdx) {
+	memcpy(sP->swapPlaceholderP, (const void*) srcEcP, sP->ecSz);
+  ECLocation *srcEclP = _getECLocation(sP, _getEntity(srcEcP));
+  void *dstEcP = _getEcPByIndex((const System*) sP, &sP->activityA[dstActIdx], dstEcIdx);
+	_mvEC(sP, dstEcP, srcEclP->activityIdx, srcEclP->ecIdx);
+	_mvEC(sP, sP->swapPlaceholderP, dstActIdx, dstEcIdx);
+}
+
+/* Deactivates component by swapping it with the last active slot and decrementing the first active index beforehand. */
+__inline static void _deactivateEC(System *sP, Entity entity) {
+  ECLocation *eclP = _getECLocation(sP, entity);
+  _swapECs(sP, eclP->ecP, eclP->activityIdx, --sP->activityA[eclP->activityIdx].firstInactiveIdx);  /* swap with last active EC */
+}
+
+/* Activates component by swapping it with the first inactive slot and incrementing the first active index afterward. */
+__inline static void _activateEC(System *sP, Entity entity) {
+  ECLocation *eclP = _getECLocation(sP, entity);
+  _swapECs(sP, eclP->ecP, eclP->activityIdx, sP->activityA[eclP->activityIdx].firstInactiveIdx++);  /* swap with last active EC */
+}
+
+/* Kind of a no-brainer that if you're transferring something, it's going to be active in its destination activity. */
+__inline static void _startEcActivity(System *sP, Entity entity, U8 dstActivity) {
+  ECLocation *eclP = _getECLocation(sP, entity);
+  if (eclP->activityIdx == dstActivity)
+    _activateEC(sP, entity);
+  else {
+    /* Move EC in destination out of the way. */
+    void *ecP = _getEcPByIndex(sP, &sP->activityA[dstActivity], sP->activityA[dstActivity].firstInactiveIdx - 1);
+    _mvEC(sP, ecP, dstActivity, sP->activityA[dstActivity].firstEmptyIdx++);  /* You should check if destination is already emtpy first. */
+  }
+}
+
+static void iniActivity(SysFP *funcPtr, Activity *aP) {
 	aP->sysFP = funcPtr;
 	aP->active = 0;
 	aP->firstInactiveIdx = 0;
 	aP->firstEmptyIdx = 0;
-	aP->cmpsA = NULL;
+	aP->ecA = NULL;
 }
 	
-Error sysNew(System **sPP, U8 sysID, const U8 activityIdx, SysFP *funcPtrA, const U8 szCmp) {
+Error sysNew(System **sPP, U8 sysID, const U8 activityIdx, SysFP *funcPtrA, const U8 ecSz) {
+  U8 nFuncs;
 	Error e = jbAlloc(sPP, sizeof(System), 1);
 	/* Allocate activity array. */
 	if (!e) {
 		SysFP *funcP = funcPtrA;
-		for (U8 nFuncs = 0; funcP != NULL; ++funcP, ++nFuncs);
+		for (nFuncs = 0; funcP != NULL; ++funcP, ++nFuncs);
 		e = arrayNew(&(*sPP)->activityA, sizeof(Activity), nFuncs);
-	}
-
-	/* These are left to parent system to allocate for better performance. */
-	(*sPP)->cmpLocationMapP = NULL;
-	(*sPP)->inboxA = NULL;
-	(*sPP)->outboxA = NULL;
+  }
+  if (!e) 
+    e = jbAlloc(&(*sPP)->swapPlaceholderP, ecSz, 1);
+  if (!e) {
+    (*sPP)->ecSz = ecSz;
+	  /* These are left to parent system to allocate for better performance. */
+	  (*sPP)->cmpLocationMapP = NULL;
+	  (*sPP)->inboxA = NULL;
+	  (*sPP)->outboxA = NULL;
+  } else {
+    /* If something bad happened, clean up. */
+    arrayDel((void**) &(*sPP)->activityA);
+    jbFree((void**) &(*sPP)->swapPlaceholderP);
+    jbFree(sPP);
+  }
 
 	return e;
 }
 
-void sysIni(System *sP, SysFP *funcPtrA) {
+void sysIni(System *sP, U8 cSz, SysFP *funcPtrA) {
 		/* Populate activity array's function pointers. */
-	if (!e) {
-		Activity *aP = &sP->activityA[0];
-		Activity *aEndP = arrayGetEndPtr(sP->activityA);
-		SysFP *funcP = funcPtrA;
-		for (; aP < aEndP; ++aP, ++funcP)
-			iniActivity(aP, funcP);
-	}
+	Activity *aP, *aEndP;
+	SysFP *funcP = funcPtrA;
+  arrayIniPtrs((void*) sP->activityA, (void**) &aP, (void**) &aEndP, -1);
+	while (aP < aEndP)
+		iniActivity(funcP++, aP++);
 	/* For performance, the parent system is in charge of allocating the component arrays within the activity array elements. */
 	/* Set individual settings. */
 	sP->active = 0;
-	sP->cmpStepSz = szCmp + sizeof(Entity);  /* Entities are squeezed between components */
+	sP->ecSz = cSz + sizeof(Entity);  /* Entities are squeezed between components */
 	sP->cmpLocationMapP = NULL;
 }
 
@@ -61,177 +138,79 @@ void sysIni(System *sP, SysFP *funcPtrA) {
 Error sysNewCmpsA(const System *sP, const U8 activityIdx, const U8 nCmps) {
 	/* Include Entity between every Component. */
 	if (_validateActivityIdx(sP, activityIdx))
-		return arrayNew(&sP->activityA[activityIdx].cmpsA, sP->cmpStepSz, nCmps);
-	return BAD_ARGS;
+		return arrayNew(&sP->activityA[activityIdx].ecA, sP->ecSz, nCmps);
+	return E_BAD_ARGS;
 }
 
-static void sysIniCmp(const System *sP, const U8 activityIdx, const U8 cmpIdx, const void *cmpP) {
-	if (_validateActivityIdx(sP, activityIdx))
-		if (_validateCmpIdx(sP, activityIdx, cmpIdx))
-			memcpy(&sP->activityA[activityIdx].cmpsA[cmpIdx], cmpP, szCmp);
+Error sysNewCmpMap(System *sP, U8 elemSz, U8 nElems) {
+  return mapNew(&sP->cmpLocationMapP, elemSz, nElems);
 }
 
-static void _sysDelActivities(const System *sP) {
+
+static void sysIniCmp(const System *sP, const U8 activityIdx, const Entity entity, const void *cmpP) {
+  if (_validateActivityIdx(sP, activityIdx)) {
+    Activity *aP = &sP->activityA[activityIdx];
+  	memcpy(_getEcPByIndex(sP, aP, aP->firstEmptyIdx++), cmpP, sP->ecSz);
+  }
+}
+
+static void _sysDelActivities(System *sP) {
 	for (U8 i = 0, nActivities = arrayGetNElems(sP->activityA); i < nActivities; i++) 
-		arrayDel(&sP->activityA[i]);
+		arrayDel((void**) &sP->activityA[i]);
 	arrayDel(&sP->activityA);
 }	
 
-Error sysNewMailbox(Msg **mailboxPP, U32 nMailSlots) {
+Error sysNewMailbox(Message **mailboxPP, U32 nMailSlots) {
 	return arrayNew(mailboxPP, sizeof(Message), nMailSlots);
 }
 		
-static void _sysClrMailbox(const Message *mailboxP) {
+static void _clrMailbox(Message *mailboxP) {
 	memset(mailboxP, 0, sizeof(Message) * arrayGetNElems(mailboxP));
 }
 
-static void _sysDelMailboxes(const System *sP) {
+static void _sysDelMailboxes(System *sP) {
 	arrayDel(&sP->inboxA);
 	arrayDel(&sP->outboxA);
 }
 
-void sysReset(const System *sP) {
-	_sysDelCmpJInners(sP);
-	_sysDelMailboxes(sP);
+void sysReset(System *sP) {
+	
 }
 
-void sysToggleActive(const System *sP) {
-	sP->active != sP->active;
+void sysToggleActive(System *sP) {
+	sP->active = !(sP->active);
 }
 
-/* Validate message before processing. */
-inline static Error _validateMessage(const System *sP, const Message *msgP, const ECSType expectedECSType, const U32 expectedID) {
-	if (msgP->toECSType != expectedECSType) 
-		return E_MSG_TO_ECS_TYPE_MISMATCH;
-	switch(msgP->toECSType) {
-		case SYSTEM:
-			if (msgP->toECSType == SYSTEM && msgP->toID != expectedID)
-				return E_MSG_TO_ID_MISMATCH;
-			if (msgP->cmd >= _MAX_SYSTEMWIDE_CMD_ID)  // every system has its own max enum for system-specific commands
-				return E_MSG_INVALID_CMD;
-			break;
-		case COMPONENT:
-			if (msgP->cmd >= sP->nFuncs)
-				return E_MSG_INVALID_CMD;
-			if (sP->activityA != NULL && msgP->cmd >= arrayGetNElems(sP->activityA[cmd]))
-				return E_MSG_INVALID_CMD;
-			break;
-		//TODO: handle messages targeting Entities; honestly, when will it be C instead of E? Isn't E more important?
-		default:
-			E_MSG_TO_ECS_TYPE_MISMATCH;
-	}
-	return SUCCESS;
+__inline static U8 _getNActivities(const System *sP) {
+  return (U8) arrayGetNElems((void*) sP->activityA);
 }
 
+
+/* This used to validate messages. Now, we boost performance validating entity behaviors externally only once at load-time instead. */
 Error sysReadMessage(System *sP, Message *msgP, U8 maxCmdID) {
-	Error e = SUCCESS;
+  ECLocation *eclP;
 	if (sP != NULL && msgP != NULL) {
 		switch(msgP->cmdType) {
 			/* Message intends a system-wide response. All systems share the same pool of systemwide functions. */
 			case SYSTEMWIDE_CMD:
-				e = _validateMessage(sP, msgP, SYSTEM, sP->id);
-				if (!e)
-					(*sysBasicFuncs[msgP->cmd])();
+			  (*sysBasicFuncs[msgP->cmd])(sP);
 				break;
 			/* Message intends a repeating function call on a component. */
 			case REPEATING_CMP_CMD:
-				e = _validateMessage(sP, msgP, COMPONENT, 0);
-				if (!e) {
-					CmpLocation *clP = _getCmpLocation(sP, msgP->toID);
-					if (clP != NULL) {
-						// if cmp is already active, you have to swap it with full knowledge of its whereabouts later.
-						if (_cmpIsActive(sP, clP)) {
-
-						} else 
-					}
-				}
+				eclP = _getECLocation(sP, msgP->toID);
+        if (eclP != NULL) {
+        
+        }
 				break;
 			/* Message intends a one-time function call on a component. */
 			case ONE_OFF_CMP_CMD:
-				e = _validateMessage(sP, msgP, COMPONENT, 0);
 				// TODO: Change this to be one-off functions. One-offs are simply toggling  activeness or moving cmp to a new loop.
 				break;
 			default:
 				break;
 		}
 	}
-}
-
-/* Returns a component's location in the jagged array of components. Each row in the J-array corresponds to a function. */
-inline static CmpLocation* _getCmpLocation(System *sP, U8 entityID) {
-	CmpLocation *clP = (CmpLocation*) mapGet(&sP->cmpAddressMap, entityID);
-}
-
-/* When activating or deactivating, you have to update the system's directory to its new location. */
-inline static void _updateCmpLocation(const System *sP, const void *cmpP, const U8 activityIdx, const U8 cmpIdx) {kk
-	Entity entity = getEntity((CmpHeader*) cmpP);
-	CmpLocation *clP = _getCmpLocation(sP, entity);  
-	clP->activityIdx = activityIdx;
-	clP->cmpIdx = cmpIdx;
-}
-
-/* Checks if the component, wherever it is in the jagged array, is before the function's stopping point in its array. */
-inline static U8 _cmpIsActive(const System *sP, const CmpLocation *clP) {
-	return sP->activityA[clP->activityIdx][clP->cmpIdx] < sP->firstInactiveIdx[clP->activityIdx];
-}
-
-inline static Entity getEntity(const CmpHeader *cmpHeaderP) {
-	return cmpHeaderP->owner;
-}
-
-inline static _mvCmp(
-
-/* TODO
- * I can see you getting into a big mess with swapping. 
- * When activating locally:
- *		swap them immediately ("swap" entails placeholding one component and then performing the move + update pair on each)
- *		update their locations with their component header's entity ID, the current row's index, and the index of each component's new index in this row
- *		increment firstInactiveIdx
- * When activating globally:
- *		move the destination row's first inactive element with the first empty slot 
- *		update its location with " "  (see similar step above)
- *		post-increment the destination row's firstEmptyIdx in the above step
- *		move the activated component from source row to the destination row's firstInactiveIdx
- *		update its location with " "  (see similar step above)
- *		post-increment the destination row's firstInactiveIdx in the above step
- *		in source row, swap the
- *		decrement the source row's firstEmptyIdx
- *
- *	It's coming clear move + update location is a unit. So perform both steps in a mvCmp() function; makes enough sense to do so. 
- *		
- * For example, you have to move components from one activity to another, but then you have to update the firstInactiveIdx in both activities, update the components' locations in their maps, and swap to fill the hole in the source activity. So before you do anything, be thoughtful about it and write it out on paper. Then after you figure it out, you'll be good to go. */
-
-/* TODO:
- *		when initializing a component, you have to allocate the size of the component plus the component's header. You also have to pass in the entity to populate the hdr->entity, and then you have to add the header size to the raw component data size. Make sure the map allocates memory with this same information. */
-/* Moves a component to a different slot without regard for contents of destination. Clears source slot after. */
-static void _mvCmp(void *srcCmpP, void *dstCmpP, U8 cSz) {
-
-}
-
-static void _swapCmps(void *c1P, void *c2P, U8 cSz) {
-	void *swapPlaceholderP;
-	Error e = jbAlloc(&swapPlaceholderP, cSz, 1);
-	if (!e) {
-		memcpy(swapPlaceholderP, c1P, cSz);
-		memcpy(c2P, c1P, cSz);
-		memcpy(c1P, swapPlaceholderP, cSz);
-	}
-}
-
-/* Deactivates component by swapping it with the last active slot and decrementing the first active index beforehand. */
-inline static void _deactivateCmp(System *sP, CmpLocation, *clP) {
-	Activity *aP = &sP->activityA[clP->activityIdx];
-	_swapCmps(&aP->cmpsA[clP->cmpIdx], 
-						&aP->cmpsA[aP->firstInactiveIdx - 1]);
-	_updateCmpLocation(sP, clP->activityIdx, --(aP->firstInactiveIdx));
-}
-
-/* Activates component by swapping it with the first inactive slot and incrementing the first active index afterward. */
-inline static void _activateCmp(System *sP, CmpLocation, *clP) {
-	Activity *aP = &sP->activityA[clP->activityIdx];
-	_swapCmps(&aP->cmpsA[clP->cmpIdx], 
-						&aP->cmpsA[aP->firstInactiveIdx + 1]);
-	_updateCmpLocation(sP, clP->activityIdx, aP->firstInactiveIdx++);
+  return SUCCESS;
 }
 
 // sysIniPtrs: move iniPtr past 1st entity and  calculate endPtr w/ firstInactiveIdx
