f23dc4f688212661c9df4be927348d259bff4c4e
bonbonbaron <bonbonbaron@gmail.com>
2021-12-03 17:10:19 -0500

    adding genes for pi0 transfer reasons

---
+++ genes/Image/biggerImg.c
@@ -0,0 +1,16 @@
+#include "genes.h"
+
+Image biggerImg = {
+	.colorMapP = &biggerCM,
+	.nColors   = 16,
+	.colorA    = &biggerColorSet[0],
+	.textureP  = NULL
+};
+
+
+XRenderC biggerXRenderC = {
+	.xHeader = {.owner = 0, .type = RENDER},
+	.imgP = &biggerImg,
+	.srcRectPP = NULL,
+	.dstRectPP = NULL
+};
---
+++ genes/include/genes.h
@@ -0,0 +1,15 @@
+#ifndef GENES_
+#define GENES_
+#include "xRender.h"
+
+// Color Palettes
+extern SDL_Color biggerColorSet[];
+
+// Bigger
+extern StripSet biggerStripSet; 
+extern StripMap biggerStripMap; 
+extern Colormap biggerCM;
+extern Image biggerImg;
+extern XRenderC biggerXRenderC;
+
+#endif
