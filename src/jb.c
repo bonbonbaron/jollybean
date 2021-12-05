b1c9e155caac5b0bee57e4ca2ed2bd5d4f64293b
bonbonbaron <bonbonbaron@gmail.com>
2021-12-04 20:38:46 -0500

    Update jb.c

--- src/jb.c
+++ src/jb.c
@@ -4,12 +4,8 @@ static System *sPA[] = {
 	&sRender
 }; 
 
-NEW_GENOME_(bigger, &biggerXRenderC.xHeader);
-GENOME_GROUP_(gg, &bigger, &bigger, &bigger);
-U16 nSystems = sizeof(sPA) / sizeof(sPA[0]);
-
 int main() {
-	Error e = xIni(sPA, nSystems, &gg);
+	Error e = xIni(sPA, sizeof(sPA) / sizeof(sPA[0]), &grp1);
 	while (!e)
 		e = xRun();
 	return e;
