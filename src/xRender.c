481520e4d092b0f371895be44a68562d535a99d4
bonbonbaron <bonbonbaron@gmail.com>
2022-10-29 02:21:16 -0500

    first draft of vectorized image loading

--- src/xRender.c
+++ src/xRender.c
@@ -8,7 +8,7 @@
 // =========================================
 // Clear color map and all its related data.
 // =========================================
-void _cmClr(ColormapS *cmP) {
+static void _cmClr(ColormapS *cmP) {
 	if (cmP != NULL) {
 	 	if (cmP->dataP != NULL)    // But if the double pointer is null, avoid any processing.
 			jbFree((void**) &cmP->dataP);
@@ -21,12 +21,185 @@ void _cmClr(ColormapS *cmP) {
 	}
 }
 
+static void _unpackStrip1BPP(U32 **srcStripPP, U32 **dstStripPP) {
+  U32 *srcStripP = *srcStripPP;
+  U32 *dstStripP = *dstStripPP;
+#ifdef __ARM_NEON__
+  // 7 instructions neon VS 40-58 instructions regular
+  asm volatile inline (
+  "vmov.u8 q10 #1\n\t"   // q10 = mask
+  );
+  // 1 x (128 pixels per quad-word)
+  for (int i = 0; i < 8; ++i) {
+    asm volatile inline (
+    "vld1.32 {d0-d1}, [%1]!\n\t"    // q0 (aka d0-d1) = packed indices
+    "vand q1, q0, q10\n\t"          // q1 = unpacked indices
+    "vst1.32 {d2-d3}, [%0]!\n\t"
+    "vshr.u8 q0, #1\n\t"            // shift q0 over 1
+    : "+r&" (dstStripP), "+r&" (srcStripP)
+    );
+  }
+#else
+  // 32 pixels per iteration, 4 iterations
+  for (int i = 0; i < 2; ++i, ++srcStripP) 
+    for (register int j = 0; j < 8; ++j)
+      *dstStripP++ =  (*srcStripP >> j) & 0x01010101;
+#endif
+}
+
+
+static void _flipStrips1BPP(ColormapS *cmP) {
+  U16 *flipEndP = cmP->stripSetP->flipSetP->flipIdxA + cmP->stripSetP->flipSetP->nFlips;
+  U32 *dstLeft4PixelsP, *dstRight4PixelsP;
+  // 4 *unpacked* pixels per U32 out of 64 pixels means there are 16 U32s.
+  for (U16 *flipIdxP = cmP->stripSetP->flipSetP->flipIdxA; flipIdxP < flipEndP; ++flipIdxP) {
+    dstLeft4PixelsP = (U32*) cmP->dataP + (*flipIdxP << 3); 
+    dstRight4PixelsP = dstLeft4PixelsP + 15;
+    for (; dstLeft4PixelsP < dstRight4PixelsP; ++dstLeft4PixelsP, --dstRight4PixelsP) {
+#ifdef __ARM_NEON__
+      asm volatile inline (
+      "vldr.64 d0, [%0]\n\t"
+      "vldr.64 d1, [%1]\n\t"
+      "vrev.8 q0\n\t"
+      "vst1.32, d0[0], [%1]\n\t"   // Neon-based swap occurs here if I can make this work right.
+      "vst1.32, d1[1], [%0]"
+      : "+r" (*dstLeft4PixelsP), "+r" (*dstRight4PixelsP)
+      );
+#else
+      // Left 4 pixels
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFFFF0000) >> 16) | ((*dstLeft4PixelsP & 0x0000FFFF) << 16);
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFF00FF00) >>  8) | ((*dstLeft4PixelsP & 0x00FF00FF) <<  8);
+      // Right 4 pixels
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFFFF0000) >> 16) | ((*dstRight4PixelsP & 0x0000FFFF) << 16);
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFF00FF00) >>  8) | ((*dstRight4PixelsP & 0x00FF00FF) <<  8);  // Put right 4 pixels into left 4 pixels
+      swap_(*dstLeft4PixelsP, *dstRight4PixelsP);
+#endif
+    }
+  }
+}
+
+static void _unpackStrip2BPP(U32 **srcStripPP, U32 **dstStripPP) {
+  U32 *srcStripP = *srcStripPP;
+  U32 *dstStripP = *dstStripPP;
+#ifdef __ARM_NEON__
+  // 7 instructions neon VS 40-58 instructions regular
+  asm volatile inline (
+  "vmov.u8 q10 #3\n\t"   // q10 = mask
+  );
+  // 1 x (64 pixels per quad-word)
+  for (int i = 0; i < 1; ++i) {  // keeping this useless loop here for when I chagne to 128-pixel strips.
+    for (int j = 0; j < 4; ++j) {  // looping through the zipped half-nibbles
+      asm volatile inline (
+      "vld1.32 {d0-d1}, [%1]!\n\t"    // q0 (aka d0-d1) = packed indices
+      "vand q1, q0, q10\n\t"          // q1 = unpacked indices
+      "vst1.32 {d2-d3}, [%0]!\n\t"
+      "vshr.u8 q0, #2\n\t"            // shift q0 over 1
+      : "+r&" (dstStripP), "+r&" (srcStripP)
+      );
+    }
+  }
+#else
+  // 32 pixels per outer loop iteration, 2 iterations
+  for (int i = 0; i < 2; ++i, ++srcStripP) 
+    for (register int j = 0; j < 8; j += 2)
+      *dstStripP++ =  (*srcStripP >> j) & 0x03030303;
+#endif
+}
+
+static void _flipStrips2BPP(ColormapS *cmP) {
+  U16 *flipEndP = cmP->stripSetP->flipSetP->flipIdxA + cmP->stripSetP->flipSetP->nFlips;
+  U32 *dstLeft4PixelsP, *dstRight4PixelsP;
+  // 4 *unpacked* pixels per U32 out of 64 pixels means there are 16 U32s.
+  for (U16 *flipIdxP = cmP->stripSetP->flipSetP->flipIdxA; flipIdxP < flipEndP; ++flipIdxP) {
+    dstLeft4PixelsP = (U32*) cmP->dataP + (*flipIdxP << 3); 
+    dstRight4PixelsP = dstLeft4PixelsP + 15;
+    for (; dstLeft4PixelsP < dstRight4PixelsP; ++dstLeft4PixelsP, --dstRight4PixelsP) {
+#ifdef __ARM_NEON__
+      asm volatile inline (
+      "vldr.64 d0, [%0]\n\t"
+      "vldr.64 d1, [%1]\n\t"
+      "vrev.8 q0\n\t"
+      "vst1.32, d0[0], [%1]\n\t"   // Neon-based swap occurs here if I can make this work right.
+      "vst1.32, d1[1], [%0]"
+      : "+r" (*dstLeft4PixelsP), "+r" (*dstRight4PixelsP)
+      );
+#else
+      // Left 4 pixels
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFFFF0000) >> 16) | ((*dstLeft4PixelsP & 0x0000FFFF) << 16);
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFF00FF00) >>  8) | ((*dstLeft4PixelsP & 0x00FF00FF) <<  8);
+      // Right 4 pixels
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFFFF0000) >> 16) | ((*dstRight4PixelsP & 0x0000FFFF) << 16);
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFF00FF00) >>  8) | ((*dstRight4PixelsP & 0x00FF00FF) <<  8);  // Put right 4 pixels into left 4 pixels
+      swap_(*dstLeft4PixelsP, *dstRight4PixelsP);
+#endif
+    }
+  }
+}
+
+
+static void _unpackStrip4BPP(U32 **srcStripPP, U32 **dstStripPP) {
+  U32 *srcStripP = *srcStripPP;
+  U32 *dstStripP = *dstStripPP;
+#ifdef __ARM_NEON__
+  // 7 instructions neon VS 40-58 instructions regular
+  asm volatile inline (
+  "vmov.u8 q10 #1\n\t"   // q10 = mask
+  );
+  // 2 x (32 pixels per quad-word)
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      asm volatile inline (
+      "vld1.32 {d0-d1}, [%1]!\n\t"    // q0 (aka d0-d1) = packed indices
+      "vand q1, q0, q10\n\t"          // q1 = unpacked indices
+      "vst1.32 {d2-d3}, [%0]!\n\t"
+      "vshr.u8 q0, #4\n\t"            // shift q0 over 1
+      : "+r&" (dstStripP), "+r&" (srcStripP)
+      );
+    }
+  }
+#else
+  // 32 pixels per iteration, 4 iterations
+  for (int i = 0; i < 2; ++i, ++srcStripP) 
+    for (register int j = 0; j < 8; ++j)
+      *dstStripP++ =  (*srcStripP >> j) & 0x01010101;
+#endif
+}
+
+static void _flipStrips4BPP(ColormapS *cmP) {
+  U16 *flipEndP = cmP->stripSetP->flipSetP->flipIdxA + cmP->stripSetP->flipSetP->nFlips;
+  U32 *dstLeft4PixelsP, *dstRight4PixelsP;
+  // 4 *unpacked* pixels per U32 out of 64 pixels means there are 16 U32s.
+  for (U16 *flipIdxP = cmP->stripSetP->flipSetP->flipIdxA; flipIdxP < flipEndP; ++flipIdxP) {
+    dstLeft4PixelsP = (U32*) cmP->dataP + (*flipIdxP << 3); 
+    dstRight4PixelsP = dstLeft4PixelsP + 15;
+    for (; dstLeft4PixelsP < dstRight4PixelsP; ++dstLeft4PixelsP, --dstRight4PixelsP) {
+#ifdef __ARM_NEON__
+      asm volatile inline (
+      "vldr.64 d0, [%0]\n\t"
+      "vldr.64 d1, [%1]\n\t"
+      "vrev.8 q0\n\t"
+      "vst1.32, d0[0], [%1]\n\t"   // Neon-based swap occurs here if I can make this work right.
+      "vst1.32, d1[1], [%0]"
+      : "+r" (*dstLeft4PixelsP), "+r" (*dstRight4PixelsP)
+      );
+#else
+      // Left 4 pixels
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFFFF0000) >> 16) | ((*dstLeft4PixelsP & 0x0000FFFF) << 16);
+      *dstLeft4PixelsP  = ((*dstLeft4PixelsP & 0xFF00FF00) >>  8) | ((*dstLeft4PixelsP & 0x00FF00FF) <<  8);
+      // Right 4 pixels
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFFFF0000) >> 16) | ((*dstRight4PixelsP & 0x0000FFFF) << 16);
+      *dstRight4PixelsP = ((*dstRight4PixelsP & 0xFF00FF00) >>  8) | ((*dstRight4PixelsP & 0x00FF00FF) <<  8);  // Put right 4 pixels into left 4 pixels
+      swap_(*dstLeft4PixelsP, *dstRight4PixelsP);
+#endif
+    }
+  }
+}
+
 // =====================================================================
 // Build color map by inflating, unpacking, and piecing together strips.
 // =====================================================================
-Error _cmGen(ColormapS *cmP) {
+static Error _cmGen(ColormapS *cmP) {
 	Error e;
-	register U32 dstFlip;
 
 	if (cmP != NULL) {
 		// Check if the image has already been reconstructed. If so, get out.
@@ -52,115 +225,65 @@ Error _cmGen(ColormapS *cmP) {
 		const U32 *cmDataP = (U32*) cmP->dataP;
 		U32 *dstStripP = (U32*) cmDataP;
 		U32 *stripSetP = (U32*) cmP->stripSetP->stripSetInfP->inflatedDataP;
+    U32 *srcStripP;
+    // Mapped stripsets need to be both unpacked and indexed. They may need strips to be flipped too.
 		switch (cmP->bpp) {
-			//TODO: case 1:
-			//TODO: case 2:
-			case 4: 
-				// Mapped stripsets need to be both unpacked and indexed. They may need strips to be flipped too.
+			case 1:
+        // First read all mapped strips into the target colormap.
 				if (cmP->stripMapP) {
 					U16 *mapEndP = ((U16*) cmP->stripMapP->stripMapInfP->inflatedDataP) + cmP->stripMapP->nIndices;
-					// 8 *packed* pixels per U32 out of 64 pixels means there are 8 U32s.
-					// First read all mapped strips into the target colormap.
-#ifdef __ARM_NEON__
-          asm("vmov.i8 q10, 0x0f\n\t")
-          asm("vmov.i8 q11, 0xf0\n\t")
-#endif
 					for (U16 *ssIdxP = (U16*) cmP->stripMapP->stripMapInfP->inflatedDataP; ssIdxP < mapEndP; ssIdxP++) {
-						U32 *srcStripP = stripSetP + (*ssIdxP << 3);
-#ifdef __ARM_NEON__
-            // 7 instructions neon VS 40-58 instructions regular
-            asm volatile inline (
-            // Pixels 01 - 32
-            "vld1.32 {d0-d1}, [%1]!\n\t"  
-            "vand q1, q0, q10\n\t"
-            "vst1.32 {d2-d3}, [%0]!\n\t"
-            // Pixels 33 - 64
-            "vld1.32 {d0-d1}, [%1]!\n\t"   
-            "vand q1, q0, q11\n\t"
-            "vshr.u8 q1, #4\n\t"
-            "vst1.32 {d2-d3}, [%0]!"
-            : "+r&" (dstStripP), "+r&" (srcStripP)
-            );
-#else
-						// Pixels 01 - 08
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 09 - 16
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 17 - 24
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 25 - 32
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 33   40
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 41   48
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 49   56
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 57   64
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-#endif
-					}
+            srcStripP = stripSetP + (*ssIdxP << 3);
+            _unpackStrip1BPP(&srcStripP, &dstStripP);
+          }
+
 					// Then flip whatever strips need flipping. Remember data's already expanded to U8s!
-					if (cmP->stripSetP->flipSetP) {
-						U16 *flipEndP = cmP->stripSetP->flipSetP->flipIdxA + cmP->stripSetP->flipSetP->nFlips;
-						U32 tmp;
-						U32 *dstLeft4PixelsP, *dstRight4PixelsP;
-						// 4 *unpacked* pixels per U32 out of 64 pixels means there are 16 U32s.
-						for (U16 *flipIdxP = cmP->stripSetP->flipSetP->flipIdxA; flipIdxP < flipEndP; flipIdxP++) {
-							for (U32 nPairsSwapped = 0; nPairsSwapped < 8; nPairsSwapped++) {
-                // TODO use vrev32.8 here.
-								// Left 4 pixels
-								dstLeft4PixelsP = (U32*) cmDataP + (*flipIdxP << 3) + nPairsSwapped; 
-								dstFlip = *dstLeft4PixelsP;
-								dstFlip   = ((dstFlip & 0xFFFF0000) >> 16) | ((dstFlip & 0x0000FFFF) << 16);
-								tmp = ((dstFlip & 0xFF00FF00) >>  8) | ((dstFlip & 0x00FF00FF) <<  8);
-								// Right 4 pixels
-								dstRight4PixelsP = dstLeft4PixelsP + 15 - (nPairsSwapped >> 1);
-								dstFlip = *dstRight4PixelsP;
-								*dstRight4PixelsP = tmp;  // Put left 4 pixels into right 4 pixels
-								dstFlip   = ((dstFlip & 0xFFFF0000) >> 16) | ((dstFlip & 0x0000FFFF) << 16);
-								*dstLeft4PixelsP = ((dstFlip & 0xFF00FF00) >>  8) | ((dstFlip & 0x00FF00FF) <<  8);  // Put right 4 pixels into left 4 pixels
-							}
-						}
-					}
+					if (cmP->stripSetP->flipSetP) 
+            _flipStrips1BPP(cmP);
 				} 
-				// Unmapped stripsets only need to be unpacked.
+				// Unmapped stripsets are already ordered, so they only need to be unpacked.
 				else {
 					U32 *srcEndP = stripSetP + (cmP->stripSetP->nStrips << 3);
-					for (U32 *srcStripP = stripSetP; srcStripP < srcEndP; srcStripP++) {
-						// Pixels 01 - 08
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 09 - 16
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 17 - 24
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 25 - 32
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 33   40
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 41   48
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 49   56
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-						// Pixels 57   64
-						*dstStripP++ =  *srcStripP   & 0x0F0F0F0F;
-						*dstStripP++ = (*srcStripP++ & 0xF0F0F0F0) >> 4;
-					}
+					for (U32 *srcStripP = stripSetP; srcStripP < srcEndP; srcStripP++) 
+            _unpackStrip1BPP(&srcStripP, &dstStripP);
+				}
+				break;
+			case 2:
+        // First read all mapped strips into the target colormap.
+				if (cmP->stripMapP) {
+					U16 *mapEndP = ((U16*) cmP->stripMapP->stripMapInfP->inflatedDataP) + cmP->stripMapP->nIndices;
+					for (U16 *ssIdxP = (U16*) cmP->stripMapP->stripMapInfP->inflatedDataP; ssIdxP < mapEndP; ssIdxP++) 
+            srcStripP = stripSetP + (*ssIdxP << 3);
+            _unpackStrip2BPP(&srcStripP, &dstStripP);
+
+					// Then flip whatever strips need flipping. Remember data's already expanded to U8s!
+					if (cmP->stripSetP->flipSetP) 
+            _flipStrips2BPP(cmP);
+				} 
+				// Unmapped stripsets are already ordered, so they only need to be unpacked.
+				else {
+					U32 *srcEndP = stripSetP + (cmP->stripSetP->nStrips << 3);
+					for (U32 *srcStripP = stripSetP; srcStripP < srcEndP; srcStripP++) 
+            _unpackStrip2BPP(&srcStripP, &dstStripP);
+				}
+				break;
+			case 4: 
+        // First read all mapped strips into the target colormap.
+				if (cmP->stripMapP) {
+					U16 *mapEndP = ((U16*) cmP->stripMapP->stripMapInfP->inflatedDataP) + cmP->stripMapP->nIndices;
+					for (U16 *ssIdxP = (U16*) cmP->stripMapP->stripMapInfP->inflatedDataP; ssIdxP < mapEndP; ssIdxP++) 
+            srcStripP = stripSetP + (*ssIdxP << 3);
+            _unpackStrip4BPP(&srcStripP, &dstStripP);
+
+					// Then flip whatever strips need flipping. Remember data's already expanded to U8s!
+					if (cmP->stripSetP->flipSetP) 
+            _flipStrips4BPP(cmP);
+				} 
+				// Unmapped stripsets are already ordered, so they only need to be unpacked.
+				else {
+					U32 *srcEndP = stripSetP + (cmP->stripSetP->nStrips << 3);
+					for (U32 *srcStripP = stripSetP; srcStripP < srcEndP; srcStripP++) 
+            _unpackStrip4BPP(&srcStripP, &dstStripP);
 				}
 				break;
 
@@ -175,6 +298,7 @@ Error _cmGen(ColormapS *cmP) {
 	return e;
 }
 
+#if 0
 // Source: https://codeincomplete.com/articles/bin-packing/
 // ========================================================
 
@@ -276,6 +400,9 @@ TaTreeNode* taNodeGrow(S32 w, S32 h) {
 // Write a insert-sort by rect w/h function that memcpys in an inner insert.
 // Write some array-based binary tree functionality -- perhaps within data.c. Only what's needed.
 // Sort all rectangles by 
+#endif
+
+
 //======================================================
 // Initialize xRender's system.
 //======================================================
