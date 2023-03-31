0fa76c79adbed651fa54aa421db0b23d39a10920
bonbonbaron <bonbonbaron@gmail.com>
2023-03-31 10:50:00 -0500

    made jb sys new able to target specific directories, improving system template now

--- templates/system.c
+++ templates/system.c
@@ -1,17 +1,34 @@
 #include "xREPLACEME.h"
 
+// TODO make this file as helpful as possible.
+// TODO add xP casting in each USED function.
+// TODO add necessary checks in each function first line.
+// TODO add map-getting wherever relevant.
+// TODO add nested map-getting wherever relevant.
+// TODO don't be annoying by asssuming unused in USED functions bro. Build out a body at least!
+// TODO Makefile and test initialization for jb script
+
+/* HOW THIS FILE IS LAID OUT:
+ *
+ *  The functions below are ordered by xMaster's calls to them.
+ *  That's so you can keep track of what will already have been done by each function call.
+ */
 //======================================================
 // Initialize REPLACEME's system.
 //======================================================
+#if 0
+XIniSysFuncDefUnused_(REPLACEME);
+#else
 Error xREPLACEMEIniSys(System *sP, void *sParamsP) {
 	unused_(sParamsP);
   unused_(sP);
   return SUCCESS;
 }
+#endif
 
-//======================================================
-// Initialize xREPLACEME's components, which are Images.
-//======================================================
+#if 0
+XIniSubcompFuncDefUnused_(REPLACEME);
+#else
 Error xREPLACEMEIniSubcomp(System *sP, void *compDataP, void *compDataSrcP) {
 	if (!sP || !compDataP || !compDataSrcP)
 		return E_BAD_ARGS;
@@ -23,23 +40,27 @@ Error xREPLACEMEIniSubcomp(System *sP, void *compDataP, void *compDataSrcP) {
 
 	return e;
 }
+#endif
 
-Error xREPLACEMEProcessMessage(System *sP, Message *msgP) {
-	unused_(sP);
-	unused_(msgP);
-	return SUCCESS;
-}
-
-XClrFuncDef_(REPLACEME) {
-  unused_(sP);
-  return SUCCESS;
-}
-
+#if 0
+XGetShareFuncDefUnused_(REPLACEME);
+#else
 XGetShareFuncDef_(REPLACEME) {
   XREPLACEME *xREPLACEMESysP = (XREPLACEME*) sP;
   Error e = SUCCESS;
   return e;
 }
+#endif
+
+#if 0
+XProcMsgFuncDefUnused_(REPLACEME)
+#else
+Error xREPLACEMEProcessMessage(System *sP, Message *msgP) {
+	unused_(sP);
+	unused_(msgP);
+	return SUCCESS;
+}
+#endif
 
 //======================================================
 // REPLACEME run function
@@ -55,6 +76,11 @@ Error xREPLACEMERun(System *sP) {
   }
 
 	return e;
+}
+
+XClrFuncDef_(REPLACEME) {
+  unused_(sP);
+  return SUCCESS;
 }
 
 //======================================================
