6d9a25e2f6fd7cf96f01104959c7951ef17676c7
Michael <bonbonbaron@gmail.com>
2024-06-05 23:23:27 -0500

    6 down, 2 to go

--- test/src/data/mail/test.c
+++ test/src/data/mail/test.c
@@ -5,7 +5,6 @@
 TAU_MAIN()
 
 typedef struct Tau {
-  Error e;
   Message *mailboxF;
   Message *fwdMailboxF;
   U32 maxNMsgs;
@@ -15,13 +14,11 @@ typedef struct Tau {
 
 TEST_F_SETUP(Tau) {
   tau->maxNMsgs = 50;
-  tau->e = mailboxNew(&tau->mailboxF, tau->maxNMsgs);
-  REQUIRE_EQ(tau->e, SUCCESS);
+  tau->mailboxF = mailboxNew( tau->maxNMsgs);
   REQUIRE_EQ(frayGetNElems_(tau->mailboxF), tau->maxNMsgs);
   REQUIRE_EQ(frayGetElemSz_(tau->mailboxF), sizeof(Message));
   REQUIRE_EQ(_frayGetNActive(tau->mailboxF), 0);
-  tau->e = mailboxNew(&tau->fwdMailboxF, tau->maxNMsgs);
-  REQUIRE_EQ(tau->e, SUCCESS);
+  tau->fwdMailboxF = mailboxNew(tau->maxNMsgs);
   REQUIRE_EQ(frayGetNElems_(tau->fwdMailboxF), tau->maxNMsgs);
   REQUIRE_EQ(frayGetElemSz_(tau->fwdMailboxF), sizeof(Message));
   REQUIRE_EQ(_frayGetNActive(tau->fwdMailboxF), 0);
@@ -36,8 +33,7 @@ TEST_F_TEARDOWN(Tau) {
 
 
 TEST_F(Tau, mailboxWrite) {
-  tau-> e = mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
-  CHECK_EQ(tau->e, SUCCESS);
+  mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
   CHECK_EQ(tau->mailboxF[0].address, 1);
   CHECK_EQ(tau->mailboxF[0].attn, 2);
   CHECK_EQ(tau->mailboxF[0].cmd, 3);
@@ -45,34 +41,21 @@ TEST_F(Tau, mailboxWrite) {
 }
 
 TEST_F(Tau, mailboxWrite_TillFull) {
-  while (!tau->e) {
-    tau->e = mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
-    if (!tau->e) {
-      U32 lastMsg = *_frayGetFirstEmptyIdxP(tau->mailboxF) - 1;
-      CHECK_EQ(tau->mailboxF[lastMsg].address, 1);
-      CHECK_EQ(tau->mailboxF[lastMsg].attn, 2);
-      CHECK_EQ(tau->mailboxF[lastMsg].cmd, 3);
-      CHECK_EQ(tau->mailboxF[lastMsg].arg, 4);
-    }
+  for (U32 i = 0; i < frayGetNElems_(tau->mailboxF); ++i) {
+    mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
+    U32 lastMsg = *_frayGetFirstEmptyIdxP(tau->mailboxF) - 1;
+    CHECK_EQ(tau->mailboxF[lastMsg].address, 1);
+    CHECK_EQ(tau->mailboxF[lastMsg].attn, 2);
+    CHECK_EQ(tau->mailboxF[lastMsg].cmd, 3);
+    CHECK_EQ(tau->mailboxF[lastMsg].arg, 4);
   }
-  CHECK_EQ(tau->e, E_FRAY_FULL);
 }
 
 TEST_F(Tau, mailboxForward) {
-  tau->e = mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
-  CHECK_EQ(tau->e, SUCCESS);
-  tau->e = mailboxForward(tau->fwdMailboxF, &tau->mailboxF[0]);
-  CHECK_EQ(tau->e, SUCCESS);
+  mailboxWrite(tau->mailboxF, 1, 2, 3, 4);
+  mailboxForward(tau->fwdMailboxF, &tau->mailboxF[0]);
   CHECK_EQ(tau->mailboxF[0].address, tau->fwdMailboxF[0].address);
   CHECK_EQ(tau->mailboxF[0].attn,    tau->fwdMailboxF[0].attn);
   CHECK_EQ(tau->mailboxF[0].cmd,     tau->fwdMailboxF[0].cmd);
   CHECK_EQ(tau->mailboxF[0].arg,     tau->fwdMailboxF[0].arg);
-}
-
-TEST_F(Tau, mailboxNew_BadArgs) {
-  tau->e = mailboxNew(NULL, 100);
-  CHECK_EQ(tau->e, E_BAD_ARGS);
-  Message *mbP = NULL;
-  tau->e = mailboxNew(&mbP, 0);
-  CHECK_EQ(tau->e, E_BAD_ARGS);
 }
